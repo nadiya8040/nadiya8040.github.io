d6541bd6e8f8e767b6d22474445613fc5c918c33
Nadiya Bloby Origia <nadiya8840903@gmail.com>
2024-02-11 15:56:24 +0800

    20240211Update
    Reduced Size with Beautifier

--- js/globalheader.js
+++ js/globalheader.js
@@ -1,12 +1,12 @@
 function showHeader() {
-    const header = document.getElementById('header');
-    const header_div = document.createElement("div");
-    header_div.classname = "globalHeader";
-    header_div.innerHTML =   
-'     <div class="title">'+
-'         <img src="./Assets/Nadiya Avatar.png" width="100" height="100" alt="Nadiverse">'+
-'             <div class="gap"></div>'+
-'     </div>';
-    header.appendChild(header_div);
+	const header = document.getElementById('header');
+	const header_div = document.createElement("div");
+	header_div.classname = "globalHeader";
+	header_div.innerHTML =   
+'	<div class="title">'+
+'		<img src="./Assets/Nadiya Avatar.png" width="100" height="100" alt="Nadiverse">'+
+'			<div class="gap"></div>'+
+'	</div>';
+	header.appendChild(header_div);
 }
 setTimeout(showHeader(),1);
