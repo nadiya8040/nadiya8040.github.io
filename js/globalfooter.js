b8a12c7f39746a993393f5cbe97c7ce294ec8360
Kirbysfan <108968369+Kirbysfan@users.noreply.github.com>
2024-09-28 18:14:13 +0800

    Update globalfooter.js

--- js/globalfooter.js
+++ js/globalfooter.js
@@ -2,26 +2,28 @@ function showFooter() {
 	const footer = document.getElementById('footer');
 	const footer_div = document.createElement("div");
 	footer_div.classname = "globalFooter";
-	footer_div.innerHTML =  
-'	<footer class="copyright">©'+
-'		<script src="./Copyright.js"></script>'+
-'			by nadiya8040, Slightly edited by'+
-'		<a href="https://twitter.com/Jirachi6774">'+
-'			Jirachi6774'+
-'		</a>. Reconstructed by'+
-'		<a href="https://kirbysfan.github.io/kfan_homepage/">'+
-'			Kirbysfan'+
-'		</a>. Favicon by'+
-'		<a href="https://cara.app/deefanatic600">'+
-'			DeeFanatic600'+
-'		</a>. Formerly created with'+
-'		<a href=https://neocities.org>'+
-'			Neocities'+
-'		</a>. Proudly hosted by'+
-'		<a href=https://github.com>'+
-'			GitHub'+
-'		</a>.'+
-'	</footer>';
+	const date = new Date();
+	let year = date.getFullYear();
+	footer_div.innerHTML =
+		'	<footer class="copyright">©' +
+		year +
+		'			by nadiya8040, Slightly edited by' +
+		'		<a href="https://twitter.com/Jirachi6774">' +
+		'			Jirachi6774' +
+		'		</a>. Reconstructed by' +
+		'		<a href="https://kirbysfan.github.io/kfan_homepage/">' +
+		'			Kirbysfan' +
+		'		</a>. Favicon by' +
+		'		<a href="https://cara.app/deefanatic600">' +
+		'			DeeFanatic600' +
+		'		</a>. Formerly created with' +
+		'		<a href=https://neocities.org>' +
+		'			Neocities' +
+		'		</a>. Proudly hosted by' +
+		'		<a href=https://github.com>' +
+		'			GitHub' +
+		'		</a>.' +
+		'	</footer>';
 	footer.appendChild(footer_div);
 }
-setTimeout(showFooter(),2);
+setTimeout(showFooter(), 2);
